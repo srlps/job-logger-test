444f62fa21467d4ca297c4ac16b91103bbc38c2b
Sergio Robles <sergio.robles@protecso.com.pe>
2020-02-13 18:38:05 -0500

    unit test working

--- src/main/java/srlps/jobloggertest/JobLogger.java
+++ src/main/java/srlps/jobloggertest/JobLogger.java
@@ -1,3 +1,5 @@
+package srlps.jobloggertest;
+
 import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
---
+++ src/main/java/srlps/jobloggertest/Main.java
@@ -0,0 +1,8 @@
+package srlps.jobloggertest;
+
+public final class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
